05abaadbcc27f7113abe975902528046f8c41d56
aylaex <aylaexdev@gmail.com>
2020-12-05 22:01:37 -0500

    completes initial styling of navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 import logo from '../images/logo.png'
+import cartIcon from '../images/shopping-cart.svg'
+
 export const Nav = () => {
   return (
     <NavWrapper>
@@ -11,16 +13,22 @@ export const Nav = () => {
           flowerbox
         </SiteTitle>
       </Brand>
+
+      <CartIconWrapper>
+        <img src={cartIcon} alt='shopping cart' />
+      </CartIconWrapper>
     </NavWrapper>
   )
 }
 
 const NavWrapper = styled.nav`
   background: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors.softWhite};
   height: ${props => props.theme.sizes.xLarge};
   margin-bottom: ${props => props.theme.sizes.medium};
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: ${props => props.theme.sizes.small};
 `
 
@@ -32,9 +40,12 @@ const LogoWrapper = styled.div`
   width: ${props => props.theme.sizes.medium};
 `
 
-const SiteTitle = styled.h1`
-  color: ${props => props.theme.colors.softWhite};
+const SiteTitle = styled.h1`#
   font-family: ${props => props.theme.fonts.primary};
   font-size: ${props => props.theme.sizes.small};
   margin-left: ${props => props.theme.sizes.small};
+`
+
+const CartIconWrapper = styled.div`
+  
 `
