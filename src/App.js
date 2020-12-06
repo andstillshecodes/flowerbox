99caf25bd31f309d60f3a73f600e85c812eccab7
aylaex <aylaexdev@gmail.com>
2020-12-05 20:47:50 -0500

    adds styled components, configures theme and global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-export default function App () {
-  return (
-    <>
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-    </>
+import 'normalize.css'
+import theme from './themes/theme'
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+
+    </ThemeProvider>
   )
 }
+
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body,
+  html {
+    font-family: ${props => props.theme.fonts.primary};
+    color: ${props => props.theme.colors.black};
+  }
+
+  a {
+    text-decoration: none;
+  }
+`
---
+++ src/themes/theme.js
@@ -0,0 +1,34 @@
+const theme = {
+  fonts: {
+    primary: 'DM Sans',
+    secondary: 'Open Sans'
+  },
+  fontWeights: {
+
+  },
+  colors: {
+    primary: '#92B2BA',
+    secondary: '#F0A8BB',
+    white: '#FFFFFF',
+    black: 'rgb(62,85,90)',
+    blackOpaque: 'rgba(62,85,90,0.5)',
+    gray: '#EEEEEE'
+  },
+  spacings: {
+    xxSmall: '.25rem',
+    xSmall: '.5rem',
+    small: '1rem',
+    medium: '2rem',
+    large: '3rem',
+    xLarge: '4rem',
+    xxLarge: '6rem'
+  },
+  breakpoints: {
+
+  },
+  shadows: {
+
+  }
+}
+
+export default theme
