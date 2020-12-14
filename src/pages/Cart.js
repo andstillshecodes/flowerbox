084c2ef4c5671f7a94542a93cb22d23f1072ce49
aylaex <aylaexdev@gmail.com>
2020-12-13 21:16:55 -0500

    adds conditional rendering for empty cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,5 @@
+import { Link } from 'react-router-dom'
 import { useContext } from 'react'
-
 import { CartContext } from '../contexts'
 
 import styled from 'styled-components'
@@ -9,23 +9,30 @@ import { CartItem } from '../components'
 
 export const Cart = () => {
   const { cart, handleEmptyCart } = useContext(CartContext)
-  console.log(cart)
 
-  return (
-    <PageLayout banner={banner}>
-      <Ul>
-        {cart.line_items?.map(item => (
-          <CartItem key={item.id} item={item} />
-        ))}
-      </Ul>
-      <PriceInfo>
-        <EmptyCart type='button' onClick={() => handleEmptyCart()}>
-          Empty Cart
-        </EmptyCart>
-        <Total>Subotal: {cart.subtotal?.formatted_with_symbol}</Total>
-      </PriceInfo>
-    </PageLayout>
-  )
+  return cart.line_items.length > 0
+    ? (
+      <PageLayout banner={banner}>
+        <Ul>
+          {cart.line_items?.map(item => (
+            <CartItem key={item.id} item={item} />
+          ))}
+        </Ul>
+        <PriceInfo>
+          <EmptyCart type='button' onClick={() => handleEmptyCart()}>
+            Empty Cart
+          </EmptyCart>
+          <Total>Subotal: {cart.subtotal?.formatted_with_symbol}</Total>
+        </PriceInfo>
+      </PageLayout>
+    )
+    : (
+      <PageLayout banner={banner}>
+        <H1>
+          Your cart is empty. Visit our <Span><Link to='/'>Flowerbox.</Link></Span>
+        </H1>
+      </PageLayout>
+    )
 }
 
 const Ul = styled.ul``
@@ -55,4 +62,14 @@ const EmptyCart = styled.div`
   &:hover {
     cursor: pointer;
   }
+`
+
+const H1 = styled.h1`
+  color: ${props => props.theme.colors.primary};
+  text-transform: none;
+`
+
+const Span = styled.span`
+  color: ${props => props.theme.colors.secondary};
+  text-decoration: underline;
 `
